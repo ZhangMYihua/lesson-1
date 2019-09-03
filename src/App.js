c70d4978fe42c34df6e45c83aa7e60e563701df7
sbrij001 <sbrij001@yahoo.com>
2019-09-03 18:37:28 -0400

    created collection-preview page && dynamically rendered for items to the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.componet';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return(
-      <div>
-        <Switch>
-          <HomePage/>
-        </Switch>
-      </div>
-    )
-  }
+function App(){
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path ='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/shop/shop.componet.jsx
+++ src/pages/shop/shop.componet.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component {
   state = {
-    collections: []
+    collections: SHOP_DATA
+  }
+
+  render(){
+    const { collections } = this.state;
+
+    return(<div className="shop-page">
+      {
+        collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+      }
+    </div>);
   }
 }
+export default ShopPage;
