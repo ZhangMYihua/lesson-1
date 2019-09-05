16d9fd6c75f71033270eed3c89e5932e06d8d999
sbrij001 <sbrij001@yahoo.com>
2019-09-05 12:41:15 -0400

    collection item image, name, and price

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
@@ -8,9 +8,9 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item,idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        .map(({id, ...otherItemProps}) => (
+          <CollectionItem key={id} {...otherItemProps}/>
+        ))};
     </div>
   </div>
 )
