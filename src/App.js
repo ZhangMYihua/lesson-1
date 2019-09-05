378f9b7ebc637327a9cccddf809f9c3fea079cca
sbrij001 <sbrij001@yahoo.com>
2019-09-05 13:04:15 -0400

    created header component with logo and links to diff pages, includes styles for header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.componet';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 function App(){
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path ='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
