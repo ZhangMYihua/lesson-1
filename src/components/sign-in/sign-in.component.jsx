4bf0740bacad285292fad6750df1769afd780f21
sbrij001 <sbrij001@yahoo.com>
2019-09-14 18:03:16 -0400

    implemented firebase auth method signInWithEmailAndPassword in handleSubmit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firbase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firbase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,10 +17,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    
+    const { email, password } = this.state;
 
-    this.setState({ email: '', password: '' });
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   handleChange = event => {
