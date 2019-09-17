cd446e522e6a6e8714af8f812eb746d341f6ecbc
sbrij001 <sbrij001@yahoo.com>
2019-09-17 19:19:22 -0400

    imported shopping cart component into header component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,10 +4,10 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => ({
+const CartIcon = () => (
   <div className='cart-icon'>
-    <ShoppingIcon className='shopping-icon'>
-    <span className='item-count'>0</span>
+    <ShoppingIcon className='shopping-icon'/>
+    <span className='item-count'> 0 </span>
   </div>
-})
+)
 export default CartIcon;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './header.styles.scss';
-import {ReactComponent as Logo } from '../../assets/crown.svg';
+
 import { auth } from '../../firebase/firbase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import {ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -22,6 +25,7 @@ const Header = ({ currentUser }) => (
         <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
         :<Link className="option" to='/signin'> SIGN IN</Link>
       }
+      <CartIcon/>
     </div>
   </div>
 )
