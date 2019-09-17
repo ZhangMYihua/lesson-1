ec05d0083e2bdc1ee1c07784f39e7544cd7c7243
sbrij001 <sbrij001@yahoo.com>
2019-09-16 22:05:01 -0400

    passed the store into the provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+
+import store from './redux/store';
 
 // provider gives react access to the reducer and the store
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
