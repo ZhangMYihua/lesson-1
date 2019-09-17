710ca776cd527e7226c67094cca53900408fb853
sbrij001 <sbrij001@yahoo.com>
2019-09-17 19:31:03 -0400

    imported cart dropdown into the header component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,11 +4,11 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => {
+const CartDropdown = () => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   </div>
-}
+)
 export default CartDropdown;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firbase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import {ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
@@ -27,6 +29,7 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon/>
     </div>
+    <CartDropdown />
   </div>
 )
 
