0522f7da13e16e097d072c47c518a3dbe5ecbea2
sbrij001 <sbrij001@yahoo.com>
2019-09-17 11:28:35 -0400

    created logic so that if there is a user signed in it will redirect to home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -44,18 +44,30 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={ () =>
+            this.props.currentUser ? (
+              <Redirect to='/' />
+            ) : (
+            <SignInAndSignUpPage />
+            )
+          }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+// used to identify if we have a user signed in
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
