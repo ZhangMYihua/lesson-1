8f3f40507e494c5a5bdc423a06614f32df2ab6cf
sbrij001 <sbrij001@yahoo.com>
2019-09-17 22:02:09 -0400

    created logic for toggling cart dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
+const CartIcon = ({ toggleCartHidden }) => (
   <div className='cart-icon'>
     <ShoppingIcon className='shopping-icon'/>
     <span className='item-count'> 0 </span>
   </div>
 )
-export default CartIcon;
+
+const mapDispatchToProps = dispatch => {
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CartIcon);
