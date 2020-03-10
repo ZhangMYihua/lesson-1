664eef2245620c7e8528bab9dffd21fc8deaedb9
Mihai Atofanei <mihai.atofanei@gmail.com>
2020-03-10 13:30:43 +1100

    created project with create react app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
+import HomePage from './homepage.component.jsx';
+
+function App () {
+
+    return <div className='App'>
+      <HomePage/> 
+    </div>;
   }
-}
 
 export default App;
