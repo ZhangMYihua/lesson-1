10cf7ed7d329522e6d2807172bdd1c9efe479eef
Mihai Atofanei <mihai.atofanei@gmail.com>
2020-04-05 11:37:14 +1000

    fixed header styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
 
-import  { ReactComponent as Logo }  from '../../assets/crown.svg'
-
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
-    <div classname='header'>
-        <Link  className='logo-container' to="/">
-           <Logo />
-        </Link>
-        <div className = 'options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
-             </Link>
-             {
-                 currentUser ? 
-                 <div className='option' onClick= {() => auth.signOut()}>SIGN OUT</div>
-                 :
-                 <Link className= 'option' to='/signin'>SIGN IN</Link>
-             }
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
+  </div>
 );
 
 export default Header;
