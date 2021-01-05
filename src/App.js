035ded189652204ed4f48b8159230846dc9617dd
Ashok Singh <ubashoksingh@in.ibm.com>
2021-01-05 20:09:06 +0530

    Created project and added file for homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return(
+      <div> 
+       <HomePage />
+      </div>
+    );
   }
 }
 
---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const HomePage = () =>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Home</h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MENS</h1>
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
